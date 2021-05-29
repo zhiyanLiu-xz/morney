48532935547c866c3bba884df16a146e3106df4f
zhiyanliu <liuzhiyan138@163.com>
2021-05-29 09:56:34 +0800

    完成 HTML 标签

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div>
+  <div id="app">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box
-  }
+ @import "~@/assets/style/reset.scss";
+ @import "~@/assets/style/helper.scss";
  body{
    line-height: 1.5;
- }
- a{
-   text-decoration: none;
-   color: inherit;
+   font-family: $font-hei;
  }
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+    color: #333;
   }
-
   #nav {
     padding: 30px;
 
