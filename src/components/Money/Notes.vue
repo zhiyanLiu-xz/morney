e33aace2144ede163dfaa636221f59fcdc2c6bff
zhiyanliu <liuzhiyan138@163.com>
2021-05-30 22:30:36 +0800

    完成 Notes 组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,15 +2,24 @@
   <div>
     <label class="notes">
       <span class="name">备注</span>
-      <input type="text" placeholder="请输入标签内容">
+      <input type="text" :value="value"
+             @input="value = $event.target.value " placeholder="请输入标签内容">
     </label>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Notes'
-  };
+  import Vue from 'vue'
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class Notes extends Vue{
+    value = '';
+    onInput(event:KeyboardEvent){
+      const input = event.target as HTMLInputElement
+      this.value = input.value
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,61 +1,102 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{output}}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
       <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="inputContent" class="zero">0</button>
+      <button @click="inputContent">.</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'NumberPad'
-  };
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class NumberPad extends Vue {
+    output = '0';
+    inputContent(event: MouseEvent) {
+      const button = (event.target as HTMLButtonElement);
+      const input = button.textContent!;
+      console.log(input)
+      if (this.output.length === 16) {return;}
+      if (this.output === '0') {
+        if ('0123456789'.indexOf(input) >= 0) {
+          this.output = input;
+        } else {
+          this.output += input;
+        }
+        return;
+      }
+      if (this.output.indexOf('.') >= 0 && input === '.') {return;}
+      this.output += input;
+    }
+
+    remove() {
+      if (this.output.length === '1') {
+        this.output = '0';
+      } else {
+        this.output = this.output.slice(0, -1);
+      }
+    }
+
+    clear() {
+      this.output = '0';
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
-  .numberPad{
-    .output{
+
+  .numberPad {
+    .output {
+      @extend %clearFix;
       @extend %innerShadow;
       text-align: right;
-      font-family: Consolas,monospace;
-      font-size: 50px;
+      font-family: Consolas, monospace;
+      font-size: 36px;
       color: #333333;
-      padding: 12px 16px;
+      padding: 9px 16px;
+      height: 72px;
     }
-    .buttons{
+
+    .buttons {
       @extend %clearFix;
-      > button{
+
+      > button {
         width: 25%;
         height: 64px;
         float: left;
         background: #f0f4f7;
         border: none;
-        &.ok{
+
+        &.ok {
           height: 64px*2;
           float: right;
         }
-        &.zero{
+
+        &.zero {
           width: 25%*2;
         }
-        &:nth-child(1){
+
+        &:nth-child(1) {
           background: #f2f2f2;
         }
-        &:nth-child(2),&:nth-child(5){
+
+        &:nth-child(2), &:nth-child(5) {
           background: #e0e0e0;
         }
       }
