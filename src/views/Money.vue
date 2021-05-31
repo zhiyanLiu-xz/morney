9daf6e42afec8aade696c7f1b50140ea3719424b
zhiyanliu <liuzhiyan138@163.com>
2021-05-31 11:13:48 +0800

    把 record 的副本保存到 localStorage

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    <NumberPad @update:value="onUpdateAmount"/>
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -14,7 +14,7 @@
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Watch} from 'vue-property-decorator';
 
   type Record = {
     tags: string[],
@@ -31,6 +31,7 @@
     record:Record = {
       tags:[],notes:'',type:'-',amount:0
     }
+    recordList:Record[] = []
     onUpdateTags(value: string[]) {
       this.record.tags = value
     }
@@ -39,6 +40,15 @@
     }
     onUpdateAmount(value: string) {
       this.record.amount = parseFloat(value)
+    }
+    saveRecord(){
+      const record2 = JSON.parse(JSON.stringify(this.record))
+      this.recordList.push(record2)
+      console.log(this.recordList)
+    }
+    @Watch('recordList')
+    onRecordChanged(){
+      window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
     }
   }
 </script>
