9e17a1f4645d51e7427dd80d1032c4883235ef69
zhiyanliu <liuzhiyan138@163.com>
2021-06-01 22:47:38 +0800

    新增 EditLabel.vue 组件

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,7 +1,7 @@
 const localStorageKeyName = 'tagList';
 type Tag = {
-  id:string
-  name:string
+  id: string
+  name: string
 }
 type TagListModel = {
   data: Tag[]
@@ -10,18 +10,18 @@ type TagListModel = {
   save: () => void
 }
 
-const tagListModel:TagListModel = {
-  data:[],
+const tagListModel: TagListModel = {
+  data: [],
   fetch() {
-    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]')
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
     return this.data;
   },
-  create(name){
-    const names = this.data.map(item => item.name)
-    if(names.indexOf(name)>=0){return 'duplicated'}
-    this.data.push({id:name,name:name})
-    this.save
-    return 'success'
+  create(name) {
+    const names = this.data.map(item => item.name);
+    if (names.indexOf(name) >= 0) {return 'duplicated';}
+    this.data.push({id: name, name: name});
+    this.save();
+    return 'success';
   },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
