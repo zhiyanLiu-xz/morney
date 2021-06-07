3a32759f63b188d48c79e948e63bae927fe61cec
zhiyanliu <liuzhiyan138@163.com>
2021-06-07 12:27:28 +0800

    可以编辑和删除标签

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,17 @@
 <template>
   <Layout>
     <div class="navBar">
-      <Icon class="leftIcon" name="left"/>
+      <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name"
+                @update:value="update"
+                field-name="标签名" placeholder="请输入标签名"/>
     </div>
     <div class="button-wrapper">
-      <Button>删除标签</Button>
+      <Button @click="remove">删除标签</Button>
     </div>
   </Layout>
 </template>
@@ -36,6 +38,19 @@
       } else {
         this.$router.replace('/404');
       }
+    }
+    update(name:string){
+      if(this.tag) {
+        tagListModel.update(this.tag.id, name)
+      }
+    }
+    remove(){
+      if(this.tag){
+        tagListModel.remove(this.tag.id)
+      }
+    }
+    goBack(){
+      this.$router.back()
     }
   }
 </script>
